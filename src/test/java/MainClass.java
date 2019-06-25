46def356b22aaa5324a5a393b237a285f6acca03
GeeJayOK <none>
2019-06-25 12:31:08 +0300

    1.0

--- src/test/java/MainClass.java
+++ src/test/java/MainClass.java
@@ -3,15 +3,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "src/resources/webdriver/linux/chromedriver");
         System.setProperty("webdriver.gecko.driver", "src/resources/webdriver/linux/geckodriver");
         HomePage homePage = new HomePage();
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
 
         driver.get(homePage.URL);
+
+        driver.quit();
 
     }
 }
