94ce5fda19b081b6d9bbc7fec966d33bdc10732f
GeeJayOK <none>
2019-06-25 10:42:48 +0300

    1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import com.yaroslav.getalo.pages.HomePage;
+
 public class Main {
     public static void main(String[] args) {
+
+        HomePage homePage = new HomePage();
+
+
 
     }
 }
--- src/main/java/com/yaroslav/getalo/pages/HomePage.java
+++ src/main/java/com/yaroslav/getalo/pages/HomePage.java
@@ -1,4 +1,12 @@
 package com.yaroslav.getalo.pages;
 
 public class HomePage {
+
+//locators
+    String URL = "https://rozetka.com.ua/ua/";
+    String SEARCH = "//input[@name='search']";
+    String SEARCH_CATEGORY = "//i[contains(text(),'iPhone Xs Max')]";
+
+
+
 }
